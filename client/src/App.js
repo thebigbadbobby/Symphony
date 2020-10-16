317dfb7f2a455409ba75dc047dce5c2d897865ff
mlpierce22 <mlpierce22@gmail.com>
2020-10-15 18:35:05 -0700

    Add material UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -10,6 +11,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Express Starter</h1>
+          {/* Sample Usage... make sure to import the button too! */}
+          <Button variant="contained" color="primary">
+            Temp Button
+          </Button>
         </header>
         <Customers />
       </div>

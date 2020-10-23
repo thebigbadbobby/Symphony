ffba1057c1a0946583ba743dea89064cd29f35c7
mlpierce22 <mlpierce22@gmail.com>
2020-10-22 22:06:40 -0700

    simplify login.styles

--- client/src/components/LogIn/LogIn.js
+++ client/src/components/LogIn/LogIn.js
@@ -1,34 +1,17 @@
 // ... Any imports you need, here are some examples:
-import GoogleLogin from 'react-google-login';
 import React from "react";
-import { Container, Typography } from '@material-ui/core';
+import { Button } from "@material-ui/core";
+import gIcon from '../../assets/google-icon.svg'
 import styles from './LogIn.styles'
-
 // To import a function from a seperate file, you would do import { functionName1, functionName2 } from './../pathToFile'
-
-  
- // Use export const so that you can also import other functions from this file
- export const LogIn = (props) => {
-   const style = styles()
+export const LogIn = (props) => {
+  const style = styles()
+  // Simply checks if we are signed in and displays a sign in or sign out button.
   return (
     <React.Fragment>
-      <Container component="div" maxWidth="lg" className={style.signInContainer}>
-        <Typography variant="h4">
-          Sign In
-        </Typography>
-        <GoogleLogin
-            className={style.login}
-            prompt='consent'
-            clientId="1086476349516-dst8kdd0jq632n50p09p7dn728sihr6j.apps.googleusercontent.com"
-            buttonText="Login with Google"
-            onSuccess={props.responseGoogle}
-            onFailure={props.authFailed}
-            hasSignedIn={true}
-            cookiePolicy={'single_host_origin'}
-          />
-      </Container>
-
+        { !props.isSignedIn ? 
+        <Button startIcon={<img src={gIcon} alt="Google"/>} className={style.signIn}variant="contained" size="large" onClick={props.handleSignIn}>Sign in with Google</Button> :
+        <Button variant="contained" color="primary" size="small" onClick={props.handleSignOut}>Sign Out</Button> }
     </React.Fragment>
-  )
- }
- 
+  );
+};

b7941f61036c74eee2b35888c60bb0284e281514
mlpierce22 <mlpierce22@gmail.com>
2020-10-22 22:07:01 -0700

    Add a toolbar to main

--- client/src/components/Main/Main.styles.js
+++ client/src/components/Main/Main.styles.js
@@ -1,6 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const styles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  signOutButton: {
+    marginLeft: "auto",
+    marginRight: theme.spacing(2)
+  }
 }));
 
 export default styles;

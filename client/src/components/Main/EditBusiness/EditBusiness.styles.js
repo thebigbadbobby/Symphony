1ae4ec8a5b88bff611baab124a739aa397b8f596
Aidan Smith <aipsmith@ucsc.edu>
2020-10-31 17:36:11 -0700

    Changed template to somewhat resemble figma. Still need to be able to fill in information.

--- client/src/components/Main/EditBusiness/EditBusiness.styles.js
+++ client/src/components/Main/EditBusiness/EditBusiness.styles.js
@@ -8,17 +8,34 @@ export const styles = makeStyles((theme) => ({
   },
   storeName: {
     fontWeight: 'bold',
+    top: '100px'
   },
   buttonContainer: {
-    // not border-width
     display: 'flex',
     flexDirection: 'column',
+    
   },
   firstButton: {
-    color: 'blue',
     borderWidth: '5px',
     borderStyle: 'solid',
-    borderColor: 'blue',
+    fontWeight: 'bold',
+    width: '400px',
+    top: '20px',
+    display: 'block',
+    textAlign: 'left',
+  },
+  saveButton: {
+    borderRadius: '22px/20px', // round sides!!!
+    fontWeight: 'bold',
+    borderStyle: 'solid',
+    backgroundColor: '#28B67E', // give button green color
+    borderColor: '#28B67E',
+    height: '45px', // sizing
+    width: '120px',
+    color: 'white', // give text white color
+    display: 'flex',
+    margin: 'auto', // center button
+    top: '35px',
   }
 }));
 

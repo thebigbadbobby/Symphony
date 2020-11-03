a2bbdaf62d42e44687a3094f22c9a1166e15118b
Hannah Yan <langqi.yan@gmail.com>
2020-11-02 19:05:56 -0800

    define python input

--- routes/routing.js
+++ routes/routing.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const twilio = require('twilio');
-const { route } = require('./business');
+const express = require("express");
+const twilio = require("twilio");
+const { route } = require("./business");
 const router = express.Router();
 
 // @description get route for one specific driver
@@ -8,35 +8,56 @@ const router = express.Router();
 // driverID
 // @payload
 // The routing info for that driver, detail TBD
-router.get('/route', (req, res) => {
-
-    res.send(req.body);
-
+router.get("/route", (req, res) => {
+  res.send(req.body);
 });
 
 // @description compute route for drivers
 // @params
-// startLocation : 'starting address'
-// list of address
-// addresses:['pickup1','dropoff1','pickup2','dropoff2'...]
-// req.body.startLocation
-// req.body.addresses
-// A: Search how to create a file using js
-// B: 
-router.post('/computeRoute', (req, res) => {
-    if (!req.body.hasOwnProperty('addresses')) {
-        res.status(400).send('Missing addresses');
-    }
-    const spawn = require('child_process').spawn;
-    const ls = spawn('python', ['./routing/computeRoute.py']);
+// req.body.startLocation -> list of strings, eg. ['starting addr1','starting addr2'...]
+// req.body.addressPairs -> list of strings, eg. ['pickup1','dropoff1','pickup2','dropoff2'...]
+router.post("/computeRoute", (req, res) => {
+  if (!req.body.hasOwnProperty("startLocation")) {
+    res.status(400).send("Missing startLocation");
+  }
 
-    ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-    ls.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-    });
-    res.status(200).send('Okay');
+  if (!req.body.hasOwnProperty("addressPairs")) {
+    res.status(400).send("Missing addressPairs");
+  }
+
+  let dict = { startLocation: req.body.startLocation };
+  dict["addressPairs"] = [];
+  let pair = {};
+  for (let i = 0; i < req.body.addressPairs.length; i += 2) {
+    let pair = {};
+    pair["pick-up-location"] = req.body.addressPairs[i];
+    pair["drop-off-location"] = req.body.addressPairs[i + 1];
+    dict["addressPairs"].push(pair);
+  }
+
+  let dictstring = JSON.stringify(dict);
+  const fs = require("fs");
+  fs.writeFile(
+    "./routing/dailyDestinationList/dests.json",
+    dictstring,
+    function (err, result) {
+      if (err) console.log("error", err);
+    }
+  );
+
+  const spawn = require("child_process").spawn;
+  const ls = spawn("python", [
+    "./routing/computeRoute.py",
+    "./routing/dailyDestinationList/dests.json",
+  ]);
+
+  ls.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  ls.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  res.status(200).send("Okay");
 });
 
 module.exports = router;

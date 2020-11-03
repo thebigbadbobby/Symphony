f0b7dd3889e0b06239046fb97610dfe2d4ebd8c5
danielbernstein <dbernstein-i@myhippo.com>
2020-11-03 12:34:00 -0800

    moved styles to seperate file

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -21,68 +21,10 @@ import SettingsIcon from '@material-ui/icons/Settings';
 import { TodaysOrders } from "./pages/TodaysOrders"
 import { OrderHistory } from "./pages/OrderHistory"
 import { StoreSettings } from "./pages/StoreSettings"
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-}));
+import styles from "./Main.styles";
 
 export const Main = (props) => {
-  const classes = useStyles();
+  const classes = styles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
--- client/src/components/Main/Main.styles.js
+++ client/src/components/Main/Main.styles.js
@@ -1,23 +1,81 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+// export const styles = makeStyles((theme) => ({
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//     [theme.breakpoints.down("xs")]: {
+//       visibility: 'hidden',
+//       fontSize: ".25rem"
+//     },
+//   },
+//   signOutButton: {
+//     marginLeft: "auto",
+//     marginRight: theme.spacing(2),
+//     [theme.breakpoints.down("xs")]: {
+//       flexGrow: 2,
+//     },
+//   }
+// }));
+
+const drawerWidth = 240;
 export const styles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  title: {
-    flexGrow: 1,
-    [theme.breakpoints.down("xs")]: {
-      visibility: 'hidden',
-      fontSize: ".25rem"
-    },
+  hide: {
+    display: 'none',
   },
-  signOutButton: {
-    marginLeft: "auto",
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.down("xs")]: {
-      flexGrow: 2,
-    },
-  }
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
 }));
 
-export default styles;
+export default styles

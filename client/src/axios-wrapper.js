de8160c4166e1a3e7fc9f8628d4439e9ad06158b
mlpierce22 <mlpierce22@gmail.com>
2020-11-10 23:12:59 -0800

    change the load balancing ip to the certified address

--- client/src/axios-wrapper.js
+++ client/src/axios-wrapper.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // See this issue for more info: https://github.com/axios/axios/issues/175#issuecomment-165521644
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://54.148.110.212'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://api.kahzum.com'
 
 export const axiosWrap = axios.create({
   baseURL,

bbf72aef9951d2cc362976b4e04267036a5a5523
mlpierce22 <mlpierce22@gmail.com>
2020-11-12 15:34:02 -0800

    get rid of the newUser and change cors thing to try and make it work

--- server.js
+++ server.js
@@ -12,27 +12,7 @@ const twilio = require('./routes/twilio');
 
 const app = express();
 
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://my.kahzum.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.use(cors({
-  origin: ['https://my.kahzum.com', 'http://localhost:3000'],
-}));
+app.use(cors({ origin: true }));
 
 // connect to mongodb & listen for requests
 
